2bb76c848adb81edbc238e4b2501f66905a68177
julia_chizhik <for.yu.ch@gmail.com>
2021-01-13 16:35:44 +0200

    summary

--- src/index.js
+++ src/index.js
@@ -3,9 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 
+// const name = 'All';
+// const element = <h1> Hello {name} { 2+2 }! </h1>
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Марья',
+  lastName: 'Моревна'
+};
+
+const element = (
+  <h1>
+    Здравствуй, {formatName(user)}!
+  </h1>
+);
 
 ReactDOM.render(
-  <h1>Hello Word!!!</h1>,
+  element,
   document.getElementById('root')
 );
 
